4fdae48a6c42da21bbfe647bf69037154c9c8b0f
kimoncar <kimoncar@yandex.ru>
2024-10-23 13:34:26 +0300

    Edit IngredientDetails

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,10 +1,13 @@
 import { FC } from 'react';
 import { Preloader } from '../ui/preloader';
 import { IngredientDetailsUI } from '../ui/ingredient-details';
+import { useParams } from 'react-router-dom';
+import { useSelector } from '../../services/store';
 
 export const IngredientDetails: FC = () => {
-  /** TODO: взять переменную из стора */
-  const ingredientData = null;
+  const params = useParams();
+  const ingredients = useSelector((state) => state.ingredients.ingredientsData);
+  const ingredientData = ingredients.find((item) => item._id === params.id);
 
   if (!ingredientData) {
     return <Preloader />;
