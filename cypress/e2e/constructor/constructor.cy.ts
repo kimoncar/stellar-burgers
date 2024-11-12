53ca6f0bf3582ceb985491a88265cc56cbe4a343
kimoncar <kimoncar@yandex.ru>
2024-11-12 16:15:17 +0300

    Add beforeEach, afterEach

--- cypress/e2e/constructor/constructor.cy.ts
+++ cypress/e2e/constructor/constructor.cy.ts
@@ -1,6 +1,24 @@
-/// <reference types="cypress" />
+describe('Проверяем конструктор бургера', function() {
+  beforeEach(() => {
+    cy.setCookie('accessToken', 'accessTokenValue');
+    localStorage.setItem('refreshToken', 'refreshTokenValue');
 
-describe('проверяем доступность приложения', function() {
+    cy.intercept('GET', `api/auth/user`, { fixture: 'dataUser.json' }).as('getUser');
+    cy.intercept('GET', `api/ingredients`, { fixture: 'dataIngredients.json' }).as('getIngredients');
+    cy.intercept('POST', `api/orders`, { fixture: 'dataOrder.json' }).as('postOrders');
+
+    cy.visit('/');
+    cy.wait('@getIngredients');
+    cy.wait('@getUser');
+  });
+
+  afterEach(() => {
+    afterEach(() => {
+      cy.clearCookie('accessToken');
+      localStorage.removeItem('refreshToken');
+    });
+  });
+
   test('сервис должен быть доступен по адресу localhost:4000', function() {
     cy.visit('http://localhost:4000');
   });
