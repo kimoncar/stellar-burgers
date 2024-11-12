68c6d9f7db53e07ed25babc218db2748ff21254d
kimoncar <kimoncar@yandex.ru>
2024-11-12 17:13:49 +0300

    Add comments

--- cypress/e2e/constructor/constructor.cy.ts
+++ cypress/e2e/constructor/constructor.cy.ts
@@ -1,22 +1,26 @@
 describe('Проверяем конструктор бургера', function() {
   beforeEach(() => {
+    // До проверок
+    // Установить тестовые токены
     cy.setCookie('accessToken', 'accessTokenValue');
     localStorage.setItem('refreshToken', 'refreshTokenValue');
 
+    // Перехватить запросы на сервер
     cy.intercept('GET', `api/auth/user`, { fixture: 'dataUser.json' }).as('getUser');
     cy.intercept('GET', `api/ingredients`, { fixture: 'dataIngredients.json' }).as('getIngredients');
     cy.intercept('POST', `api/orders`, { fixture: 'dataOrder.json' }).as('postOrders');
 
+    // Открыть страницу и дождаться данные
     cy.visit('/');
     cy.wait('@getIngredients');
     cy.wait('@getUser');
   });
 
   afterEach(() => {
-    afterEach(() => {
-      cy.clearCookie('accessToken');
-      localStorage.removeItem('refreshToken');
-    });
+    // После проверок
+    // Удалить тестовые токены
+    cy.clearCookie('accessToken');
+    localStorage.removeItem('refreshToken');
   });
 
   test('сервис должен быть доступен по адресу localhost:4000', function() {
