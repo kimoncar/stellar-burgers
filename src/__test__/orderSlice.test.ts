4c6b828b6213697e3bb785562abc1693507b2a1d
kimoncar <kimoncar@yandex.ru>
2024-11-15 13:29:46 +0300

    Add selectors tests for orderSlice

--- src/__test__/orderSlice.test.ts
+++ src/__test__/orderSlice.test.ts
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import orderSlice, { getOrderThunk, initialState } from "../slices/orderSlice";
+import orderSlice, { getOrderSelector, getOrderThunk, initialState, isLoadingOrderSelector } from "../slices/orderSlice";
 
 // Хранилище для тестов
 const testStore = () => configureStore({
@@ -9,6 +9,27 @@ const testStore = () => configureStore({
 });
 
 describe('Проверяем orderSlice', () => {
+  const testResponseData = {
+    "success": true,
+    "orders": [
+      {
+        "_id": "67371782b27b06001c3e8514",
+        "ingredients": [
+          "643d69a5c3f7b9001cfa093d",
+          "643d69a5c3f7b9001cfa093e",
+          "643d69a5c3f7b9001cfa093d"
+        ],
+        "owner": "671a211ad829be001c7785ad",
+        "status": "done",
+        "name": "Флюоресцентный люминесцентный бургер",
+        "createdAt": "2024-11-15T09:42:26.796Z",
+        "updatedAt": "2024-11-15T09:42:27.786Z",
+        "number": 59471,
+        "__v": 0
+      }
+    ]
+  };
+
   describe('Проверяем исходное состояние', () => {
     it('[#1] - тест исходного состояния', () => {
       const store = testStore();
@@ -17,27 +38,6 @@ describe('Проверяем orderSlice', () => {
   });
 
   describe('Проверяем дополнительные редьюсеры (extraReducers)', () => {
-    const testResponseData = {
-      "success": true,
-      "orders": [
-        {
-          "_id": "67371782b27b06001c3e8514",
-          "ingredients": [
-            "643d69a5c3f7b9001cfa093d",
-            "643d69a5c3f7b9001cfa093e",
-            "643d69a5c3f7b9001cfa093d"
-          ],
-          "owner": "671a211ad829be001c7785ad",
-          "status": "done",
-          "name": "Флюоресцентный люминесцентный бургер",
-          "createdAt": "2024-11-15T09:42:26.796Z",
-          "updatedAt": "2024-11-15T09:42:27.786Z",
-          "number": 59471,
-          "__v": 0
-        }
-      ]
-    };
-
     describe('Проверяем getOrderThunk', () => {
       it('[#1] - тест ожидания ответа (getOrderThunk.pending)', () => {
         const store = testStore();
@@ -71,6 +71,22 @@ describe('Проверяем orderSlice', () => {
         expect(store.getState().order.error).toBeNull();
         expect(store.getState().order.order).toEqual(testResponseData.orders[0]);
       });
+    });
+  });
+
+  describe('Проверяем селекторы', () => {
+    const store = testStore();
+    store.dispatch({
+      type: getOrderThunk.fulfilled.type,
+      payload: testResponseData
+    });
+
+    it('[#1] - тест селектора getOrderSelector', () => {
+      expect(getOrderSelector(store.getState())).toEqual(testResponseData.orders[0]);
+    });
+    
+    it('[#2] - тест селектора isLoadingOrderSelector', () => {
+      expect(isLoadingOrderSelector(store.getState())).toBeFalsy();
     });
   });
 });
